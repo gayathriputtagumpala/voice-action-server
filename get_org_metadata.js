0e3789c9743a20332a1e65144f1fbbfa8adb4b70
gayathriputtagumpala <gayathriputtagumpala@gmial.com>
2026-05-08 18:15:57 +0530

    update

---
+++ get_org_metadata.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const https = require('https');
+
+async function test() {
+  const url = 'https://fa-eubg-test-saasfademo1.ds-fa.oraclepdemos.com/hcmRestApi/resources/11.13.18.05/organizations?q=OrganizationId%3D300000047013620&onlyData=true';
+  
+  const agent = new https.Agent({ rejectUnauthorized: false });
+  const auth = 'Basic dXNlcl9yMTRfYTJmOnFvMkgqNlcj';
+  
+  try {
+    const res = await axios.get(url, {
+      httpsAgent: agent,
+      headers: {
+        'Authorization': auth,
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(JSON.stringify(res.data.items[0], null, 2));
+  } catch (err) {
+    console.error(err.response?.data || err.message);
+  }
+}
+
+test();
